76f9fbb911a1db3fad624c056efd7269cfd9c0c9
pakim249CAL <70043880+pakim249CAL@users.noreply.github.com>
2021-08-02 19:15:26 -0700

    Update ChainChainlinkAggregator.sol
    Adding a division by zero check and using updated solidity to remove need for SafeMath

--- ChainChainlinkAggregator.sol
+++ ChainChainlinkAggregator.sol
@@ -1,5 +1,9 @@
-pragma solidity ^0.6.2;
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity ^0.8.6;
 
+/*
+* The interface AAVE's Oracle uses
+*/
 interface IChainlinkAggregator {
   function latestAnswer() external view returns (int);
   function decimals() external view returns (uint8);
@@ -10,7 +14,6 @@ interface IChainlinkAggregator {
 * and has the interface needed for AAVE to interact with it.
 * For price feeds A -> B and C -> B, outputs A -> C
 */
-
 contract ChainChainlinkAggregator is IChainlinkAggregator {
 
     address internal priceFeedAddress1;
@@ -21,20 +24,17 @@ contract ChainChainlinkAggregator is IChainlinkAggregator {
     /**
      * 
      */
-    constructor(address p1, address p2) public {
+    constructor(address p1, address p2) {
         priceFeedAddress1 = p1;
         priceFeedAddress2 = p2;
         priceFeed1 = IChainlinkAggregator(p1);
         priceFeed2 = IChainlinkAggregator(p2);
     }
     
-    /**
-     * Returns the latest price
-     */
-    function getPrice1() public view returns (int) {
+    function getPrice1() external view returns (int) {
         return priceFeed1.latestAnswer();
     }    
-    function getPrice2() public view returns (int) {
+    function getPrice2() external view returns (int) {
         return priceFeed2.latestAnswer();
     }
     function getPriceSource1() public view returns (address) {
@@ -43,18 +43,26 @@ contract ChainChainlinkAggregator is IChainlinkAggregator {
     function getPriceSource2() public view returns (address) {
         return priceFeedAddress2;
     }
+    
+    function getRoundNumber() public view returns (int) {
+        return priceFeed1.
+    }
     /**
      * Converts asset A price to asset C price using two feeds and returns 18 decimal point precision
      */
-    function latestAnswer() public view virtual override returns (int) {
-        int price1 = getPrice1();
+    function latestAnswer() external view virtual override returns (int) {
+        int price1 = getPrice1;
+        int price2 = getPrice2;
         uint8 price1d = priceFeed1.decimals();
-        int price2 = getPrice2();
         uint8 price2d = priceFeed2.decimals();
-        int newprice = price1  * (int(10) ** (price2d - price1d + uint8(18))) / price2;
-        return newprice;
+        if (price2 > 0) {
+            return (price1 * int(10 ** (price2d - price1d + uint8(18))) / price2);
+        }
+        else {
+            return 0;
+        }
     }
-    function decimals() public view virtual override returns (uint8) {
+    function decimals() external view virtual override returns (uint8) {
         return uint8(18);
     }
 }
